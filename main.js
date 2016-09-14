bd380b015b20a3e9e812e19df72fb4b5cb520c04
Richard Mands <richard@eduku.org>
2016-09-13 22:31:54 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(init) 
-var number = 0;
+
 var array = [];
 var finalArray =[];
 
@@ -12,36 +12,23 @@ function init() {
 
 function addButtonClicked() {
 
-  number = number + 1;
-  console.log(number);
-
-  console.log('Clicked!');
-
   var $input = $('#name');
 
   var name = $input.val();
 
-  console.log('name: ', name);
-
   var newArray = name.split(',')
-  console.log('NewArray: ', newArray)
 
   for (i=0; i<newArray.length; i++){
     array.push(newArray[i])
   }
-
-  console.log('Array ', array);
   
   $input.val('');
 
   $('#list').html("");
   for (i=0; i<array.length; i++){
     var $li = $('<li>');
-    var $input2 = $('#color');
-    var color = $input2.val();
-    $li.text(array[i]).css('color', color).attr('id',number);
+    $li.text(array[i]);
     var $list = $('#list');
-
     $list.append($li);
     
   }
@@ -51,19 +38,14 @@ function addButtonClicked() {
 
 function  randomizerButtonClicked() {
 
-  console.log('Randomizer Clicked');
-
   var randomNumber = Math.floor((Math.random() * array.length));
 
-  console.log('Random Number ', randomNumber);
   $('#random').text("Hello " + array[randomNumber]);
 }
 
 
 function teamerizerButtonClicked() {
   finalArray = array.slice()
-  console.log("Teamerizer is clicked!")
-  console.log("Array: ", array);
   $('#teams').html("Hello Teams!");
   for (i = finalArray.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
@@ -71,20 +53,13 @@ function teamerizerButtonClicked() {
     finalArray[i] = finalArray[j];
     finalArray[j] = temp;
   }
-  console.log("Final Array: ", finalArray);
-  console.log("Array Randomized");
-  console.log("Array: ",array);
-
-
 
   var n = $('#teamSize').val();
   var res = [];
-  console.log(array);
   function split() {
     while (finalArray.length) {
       res.push(finalArray.splice(0, n));
     }
-    console.log(res);
   }
   split();
 
@@ -95,8 +70,6 @@ function teamerizerButtonClicked() {
     var $list = $('#teams');
     $list.append($li);
   }
-  // array = finalArray;
-  console.log(array);
 }
 
 
